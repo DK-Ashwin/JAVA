6188e10401468af5405f8f8f711b86fbbeb09ffc
Ashwin_DK <ashwindilleep71409@gmail.com>
2026-07-10 19:38:06 +0530

    Fix Armstrong number program implementation
    Refactored the Armstrong number check logic and fixed syntax errors.

--- amstrong.java
+++ amstrong.java
@@ -1,25 +1,19 @@
-//Implement a java program to check a given number is Armstrong number or not.
 import java.util.Scanner;
-public class amstrong{
-	public static void main(String arg[]){
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the number : ");
-		int n=sc.nextInt();
-		int temp=n;
-		int order=;
-		int sum_digits=0;
-		int i;
-		for(i=0;i<=n:i++){
-			int sum_digits=sum_digits+i**order;
-		}
-		if sum_digits==temp{
-			System.out.print("Amstrong number");
-		}
+	public class armstrong{
+		public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter number : ");
+		int n = sc.nextInt();
+		int org;
+		org = n;
+		int r=0,temp;
+		while(n>0){
+			temp=n%10;
+			r=r+temp*temp*temp;
+			n=n/10;}
+		if(r == org){
+			System.out.println("armstrong");}
 		else{
-			System.out.print("Not Amstrong number");
+			System.out.println("not armstrong");}
 		}
-	}
 }
-order = len(str(n))
-
-
